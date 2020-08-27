0a1019bd9c053d4f5173b9d87e8b6b5d2bb1e431
unknown <18ucs081@lnmiit.ac.in>
2020-08-27 15:14:41 +0530

    T and F

---
+++ src/F.js
@@ -0,0 +1,10 @@
+// Function
+// * → Boolean
+// Returns Boolean
+
+/*
+A function that always returns false. Any passed in parameters are ignored.
+See also T.
+
+R.F(); //=> false
+*/
---
+++ src/T.js
@@ -0,0 +1,10 @@
+// Function
+// * → Boolean
+// Returns Boolean
+
+/*
+A function that always returns true. Any passed in parameters are ignored.
+See also F.
+
+R.T(); //=> true
+*/
